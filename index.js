ad17ddd2f006bdde4fa47224468623a92deeec67
KevinDoncel20 <131317968+KevinDoncel20@users.noreply.github.com>
2023-04-27 12:32:17 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/* funciones declaradas*/
+
+console.log("funciones declaradas")
+
+function saludar(nombre){
+    console.log("hola este es un saludo" + $(nombre))
+}
+
+
+//llmarla
+saludar("Diego")
+
+
+
+/* funciones expresadas*/
+
+/* funciones flecha*/
